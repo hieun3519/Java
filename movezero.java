f693c3a00caeedfef8ca3ac811d410fdc038a3e6
Hieu Nguyen <hieun3519@gmail.com>
2023-03-07 13:15:37 -0600

    snowball method

--- movezero.java
+++ movezero.java
@@ -1,3 +1,23 @@
 public class movezero {
+    public static void moveZeroes(int[] nums){
+
+        int snowBall =0;
+        for(int i=0; i <nums.length; i++){
+            if(nums[i] == 0){
+                snowBall++;
+            }
+            else if(snowBall >0){
+                int t = nums[i];
+                nums[i] =0;
+                nums[i-snowBall] =t;
+            }
+            }
+
+    }
+
+    public static void main(String[] args){
+        int[] nums = {0,1,0,3,12};
+        moveZeroes(nums);
+    }
 
 }
